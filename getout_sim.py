93bc1e77dabefa385a3b7c13e5b5ce7371a15fd4
Samuel Spencer <sspencer@wgs31.zeuthen.desy.de>
2019-06-26 15:18:53 +0200

    Modified sim code

--- getout_sim.py
+++ getout_sim.py
@@ -13,12 +13,24 @@ ROOT.ROOT.EnableImplicitMT()
 if gSystem.Load("$EVNDISPSYS/lib/libVAnaSum.so"):
     print("Problem loading EventDisplay libraries - please check this before proceeding")
 
-gammafile='/lustre/fs19/group/cta/users/sspencer/ver/testgamma.dst.root'
-protonfile=''
+gammafile='/lustre/fs19/group/cta/users/sspencer/ver/aux/testgamma.dst.root'
+protonfile='/lustre/fs19/group/cta/users/sspencer/ver/aux/testproton.dst.root' #This is a test tycho run just to use as a placeholder until we get some MC
 cam = 'VERITAS'
-runcode=64080
+runcode='Testmixture'
 nochannels=499
-notrigs=100
+
+f=ROOT.TFile.Open(protonfile,'read')
+mytree=f.Get("dst")
+noprotons=len(tree2array(mytree,branches=['eventNumber']))
+f.Close()
+
+f=ROOT.TFile.Open(gammafile,'read')
+mytree=f.Get("dst")
+nogammas=len(tree2array(mytree,branches=['eventNumber']))
+f.Close()
+
+notrigs=min([noprotons,nogammas])
+print('Number of triggers',notrigs)
 
 mappers = {}
 
@@ -40,18 +52,19 @@ def dedead(imarr,tel,pixel):
         elif imarr[i][0][tel][pixel+3] !=0.0 and imarr[i][0][tel][pixel-3] !=0.0:
             imarr[i][0][tel][pixel]=(imarr[i][0][tel][pixel+3]+imarr[i][0][tel][pixel-3])/2.0
         else:
-            print('Dead pixel not corrected: '+str(k))
+            print('Dead pixel not corrected: '+str(pixel))
     except IndexError:
         print('Index error')
     return imarr
 
 def extract_im(mappers,i,method,deadarr,imarr,tel,cam):
     '''Extracts VERITAS images from root files and performs hexagonal image manipulation.'''
-    deadvals=deadarr[i][0][tel][:499]
-    deadvals=np.expand_dims(deadvals,1)
-    deadlist=np.where(deadvals!=0)[0]
-    for k in deadlist:
-        imarr=dedead(imarr,tel,k)
+    if deadarr is not None:
+        deadvals=deadarr[i][0][tel][:499]
+        deadvals=np.expand_dims(deadvals,1)
+        deadlist=np.where(deadvals!=0)[0]
+        for k in deadlist:
+            imarr=dedead(imarr,tel,k)
     image=imarr[i][0][tel][:499]
     image=np.expand_dims(image,1)
     image = mappers[method].map_image(image, cam)
@@ -62,8 +75,8 @@ for method in hex_methods:
     mapping_method = {cam: method for cam in hex_cams}
     mappers[method] = ImageMapper(mapping_method=mapping_method)
 
+print('Processing Protons')
 f=ROOT.TFile.Open(protonfile,'read')
-
 mytree=f.Get("dst")
 
 imarr=tree2array(mytree,branches=['sum'],start=0,stop=notrigs)
@@ -76,7 +89,7 @@ for i in np.arange(notrigs):
     to_hdf['isGamma'].append(0) #0 is proton, 1 is gamma
 
     for method in hex_methods:
-        image1=extract_im(mappers,i,method,deadarr,imarr,0,cam)
+        image1=extract_im(mappers,i,method,deadarr,imarr,0,cam) #Should set deadarr to None when MC comes through
         image2=extract_im(mappers,i,method,deadarr,imarr,1,cam)
         image3=extract_im(mappers,i,method,deadarr,imarr,2,cam)
         image4=extract_im(mappers,i,method,deadarr,imarr,3,cam)
@@ -89,24 +102,24 @@ for i in np.arange(notrigs):
 
 f.Close()
 
+print('Processing Gammas')
 f=ROOT.TFile.Open(gammafile,'read')
 
 mytree=f.Get("dst")
 
 imarr=tree2array(mytree,branches=['sum'],start=0,stop=notrigs)
-deadarr=tree2array(mytree,branches=['dead'],start=0,stop=notrigs)
 evarr=tree2array(mytree,branches=['eventNumber'],start=0,stop=notrigs)
 
 for i in np.arange(notrigs):
 
-    to_hdf['id'].append(evarr[i][0])
+    to_hdf['id'].append(evarr[i][0]+notrigs)
     to_hdf['isGamma'].append(1)
 
     for method in hex_methods:
-        image1=extract_im(mappers,i,method,deadarr,imarr,0,cam)
-        image2=extract_im(mappers,i,method,deadarr,imarr,1,cam)
-        image3=extract_im(mappers,i,method,deadarr,imarr,2,cam)
-        image4=extract_im(mappers,i,method,deadarr,imarr,3,cam)
+        image1=extract_im(mappers,i,method,None,imarr,0,cam)
+        image2=extract_im(mappers,i,method,None,imarr,1,cam)
+        image3=extract_im(mappers,i,method,None,imarr,2,cam)
+        image4=extract_im(mappers,i,method,None,imarr,3,cam)
         out_arr=np.zeros((4,np.shape(image1)[0],np.shape(image1)[1],1),dtype='float32')
         out_arr[0,:,:,:]=image1
         out_arr[1,:,:,:]=image2
@@ -119,13 +132,17 @@ f.Close()
 randomize = np.arange(len(to_hdf['isGamma'])) #Randomization Code
 np.random.shuffle(randomize)
 
+print('Writing HDF5')
 h5file = tables.open_file(str(runcode)+'_processed.hdf5', mode="w")
 root = h5file.root
 
 for keyval in to_hdf.keys():
-    to_hdf[keyval]=np.asarray(to_hdf[keyval],dtype='float32')
+    if keyval=='id' or keyval=='isGamma':
+        to_hdf[keyval]=np.asarray(to_hdf[keyval],dtype='int32')
+    else:
+        to_hdf[keyval]=np.asarray(to_hdf[keyval],dtype='float32')
     to_hdf[keyval]=to_hdf[keyval][randomize]
-    h5file.create_array(root,keyval,np.float32(to_hdf[keyval]),keyval)
+    h5file.create_array(root,keyval,to_hdf[keyval],keyval)
 
 h5file.close()
 
