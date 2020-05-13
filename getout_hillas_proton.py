e28f9435210c1b2b68ff1c6a267ec532aa2ccab0
Samuel Spencer <sspencer@wgs18.zeuthen.desy.de>
2020-05-13 20:53:21 +0200

    Working hillas analysis on real data

--- getout_hillas_proton.py
+++ getout_hillas_proton.py
@@ -12,18 +12,34 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
+def dedead(imarr,tel,pixel):
+    '''Attempts to correct for dead pixels by setting them equal to the mean of the 1st 2nd or 3rd nearest pair of non-zero pixels'''
+    try:
+        if imarr[i][0][tel][pixel+1] !=0.0 and imarr[i][0][tel][pixel-1] !=0.0:
+            imarr[i][0][tel][pixel]=(imarr[i][0][tel][pixel+1]+imarr[i][0][tel][pixel-1])/2.0
+        elif imarr[i][0][tel][pixel+2] !=0.0 and imarr[i][0][tel][pixel-2] !=0.0:
+            imarr[i][0][tel][pixel]=(imarr[i][0][tel][pixel+2]+imarr[i][0][tel][pixel-2])/2.0
+        elif imarr[i][0][tel][pixel+3] !=0.0 and imarr[i][0][tel][pixel-3] !=0.0:
+            imarr[i][0][tel][pixel]=(imarr[i][0][tel][pixel+3]+imarr[i][0][tel][pixel-3])/2.0
+        else:
+            pass
+            #print('Dead pixel not corrected: '+str(pixel))
+    except IndexError:
+        print('Index error')
+    return imarr
+
 ROOT.ROOT.EnableImplicitMT()
 ROOT.gROOT.SetBatch(True)
 if gSystem.Load("$EVNDISPSYS/lib/libVAnaSum.so"):
     print("Problem loading EventDisplay libraries - please check this before proceeding")
 
-protonfile='/lustre/fs19/group/cta/users/sspencer/ver/aux/mcgammacrab.dst.root' #This is a test tycho run just to use as a placeholder until we get some MC
+#protonfile='/lustre/fs19/group/cta/users/sspencer/ver/aux/mcgammacrab.dst.root' #This is a test tycho run just to use as a placeholder until we get some MC
+protonfile='/lustre/fs19/group/cta/users/sspencer/ver/eventdisplay_output/64080.dst.root'
 cam = 'VERITAS'
-particle='Gamma2'
-runcode='Crabrun2_hillas'
-runname='/lustre/fs19/group/cta/users/sspencer/Crabrun2/'+runcode+'_'
+particle='Real'
 nochannels=499
 nofiles=10
+Realdata=True
 
 f=ROOT.TFile.Open(protonfile,'read')
 mytree=f.Get("dst")
@@ -31,13 +47,23 @@ noprotons=len(tree2array(mytree,branches=['eventNumber']))
 f.Close()
 
 notrigs=noprotons
+
 print('Number of triggers',notrigs)
 
-def extract_hillas(i,imarr,tel):
+def extract_hillas(i,imarr,tel,Realdata,deadvals):
     '''Extracts VERITAS images from root files and performs hexagonal image manipulation.'''
     #print(imarr[i][0][tel][:499])
     try:
-        image=imarr[i][0][tel][:499]
+        if Realdata==True:
+            deadvals=deadarr[i][0][tel][:499]
+            deadvals=np.expand_dims(deadvals,1)
+            deadlist=np.where(deadvals!=0)[0]
+            for k in deadlist:
+                imarr=dedead(imarr,tel,k)
+            image=imarr[i][0][tel][:499]
+        else:
+            image=imarr[i][0][tel][:499]
+
     except IndexError:
         return None
 
@@ -76,25 +102,29 @@ t4i=[]
 for runno in np.arange(nofiles):
     to_hdf={'id':[],'isGamma':[],'oversampling':[],'rebinning':[],'nearest_interpolation':[],'bilinear_interpolation':[],'bicubic_interpolation':[],'image_shifting':[],'axial_addressing':[]}
     startev=runno*trigsperfile
-    stopev=(runno+1)*trigsperfile
+    if runno==nofiles:
+        stopev=notrigs
+    else:
+        stopev=(runno+1)*trigsperfile
     print('Processing Protons')
 
     f=ROOT.TFile.Open(protonfile,'read')
     mytree=f.Get("dst")
     print('Proton DST Retrieved')
     imarr=tree2array(mytree,branches=['sum'],start=startev,stop=stopev)
-    #deadarr=tree2array(mytree,branches=['dead'],start=startev,stop=stopev)
+    if Realdata==True:
+        deadarr=tree2array(mytree,branches=['dead'],start=startev,stop=stopev)
+    else:
+        deadarr=None
     evarr=tree2array(mytree,branches=['eventNumber'],start=startev,stop=stopev)
     
     for i in np.arange(trigsperfile):
         
-        to_hdf['id'].append(trigsperfile*runno+evarr[i][0])
-        to_hdf['isGamma'].append(0) #0 is proton, 1 is gamma
 
-        h1=extract_hillas(i,imarr,0) #Should set deadarr to None when MC comes through
-        h2=extract_hillas(i,imarr,1)
-        h3=extract_hillas(i,imarr,2)
-        h4=extract_hillas(i,imarr,3)
+        h1=extract_hillas(i,imarr,0,Realdata,deadarr) #Should set deadarr to None when MC comes through
+        h2=extract_hillas(i,imarr,1,Realdata,deadarr)
+        h3=extract_hillas(i,imarr,2,Realdata,deadarr)
+        h4=extract_hillas(i,imarr,3,Realdata,deadarr)
         if h1 is not None:
             t1i.append(h1[4][0])
             t2i.append(h2[4][0])
@@ -184,4 +214,3 @@ plt.legend()
 plt.xlabel('Value')
 plt.ylabel('Frequency (counts)')
 plt.savefig('../Figures/'+particle+'_hk.png')
-raise KeyboardInterrupt
