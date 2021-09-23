33134a72bca7785ed917dec252e611f27ce7413e
STSpencer <sojourner92@gmail.com>
2021-09-23 14:09:21 +0100

    Updated event plotters for thesis

--- realplotter.py
+++ realplotter.py
@@ -2,23 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import h5py
 
-datafile=h5py.File('/store/spencers/Data/Crab64080/Crabrun2_132_SIM.hdf5','r')
+datafile=h5py.File('/store/spencers/Data/Real64080/64080_44_REAL.hdf5','r')
 plotmode='oversampling'
 print(datafile.keys)
-isGam=datafile['isGamma'][:].tolist()
+#isGam=datafile['isGamma'][:].tolist()
 ids=datafile['id'][:].tolist()
 
 images=datafile[plotmode][:]
 print(np.shape(images))
 datafile.close()
 print(ids)
-for i in np.arange(len(isGam)):
+for i in np.arange(np.shape(images)[0]):
     fig,axs=plt.subplots(2,2)
+    '''
     if isGam[i]==1:
-        plt.suptitle('Simulated $\gamma$-Ray, Event Number: '+str(ids[i]))
+        plt.suptitle('$\gamma$-ray, event number: '+str(ids[i]))
     else:
-        plt.suptitle('Simulated Proton, Event Number: '+str(ids[i]))
-    #plt.suptitle('Real Event, Event Number: '+str(ids[i]))
+        plt.suptitle('Proton, event number: '+str(ids[i]))
+    '''
+    plt.suptitle('Real Event, Event Number: '+str(ids[i]))
     im1=axs[0,0].imshow(images[i,0,:,:,0])
     plt.colorbar(im1,ax=axs[0,0],label='Intensity (p.e.)')
     axs[0,0].set_xlabel('x (Pixels)')
@@ -36,6 +38,6 @@ for i in np.arange(len(isGam)):
     axs[1,1].set_ylabel('y (Pixels)')
     plt.colorbar(im4,ax=axs[1,1],label='Intensity (p.e.)')
     plt.tight_layout()
-    plt.savefig('/store/spencers/eventplots/sim_'+str(i)+'_oversampling.png')
+    plt.savefig('/store/spencers/eventplots/realevent_'+str(i)+'_oversampling.png')
     plt.clf()
     plt.cla()
